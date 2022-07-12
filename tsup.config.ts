a3dd0f7bec977b31e61082cc18342563a20cf6de
Eike Foken <eike.foken@aperto.com>
2022-07-12 13:46:34 +0200

    build: add entrypoint for types

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
         'src/index.ts',
         'src/components/index.ts',
         'src/primitives/index.ts',
+        'src/types/index.ts',
         'src/utils/index.ts',
     ],
     format: ['esm', 'cjs'],
